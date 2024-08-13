7de88a564affbc5d5ceff944cc89eeba4b8cfea3
Bilal <bilalbilin12@gmail.com>
2024-08-13 18:16:59 +0300

    Add project files.

---
+++ PatikaWeek5PracticeTry-Catch/Program.cs
@@ -0,0 +1,27 @@
+﻿
+bool isValidInput = false;
+
+while (!isValidInput) // Kullanıcıdan doğru formatta veri alana kadar sormak için while döngüsü kullanıldı.
+{
+    try // Hata alınabilecek yer try bloğuna konuldu
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz: ");
+        
+        string input = Console.ReadLine(); 
+
+        int number = Convert.ToInt16(input); // Girilen değer int türüne dönüştürülür
+
+        Console.WriteLine($"Girdiğiniz sayının karesi -->{Math.Pow(number, 2)}"); // Hata alınmazsa konsola yazdırılacak metin
+
+        isValidInput = true; // Yukarıda hatalara takılmazsa döngüden çıkacak koşul
+    }
+    catch(FormatException ex) // Format hatasının yakalandığı catch bloğu
+    {
+        Console.WriteLine("Yanlış formatta bir veri girdiniz.");
+    }
+    catch(Exception ex) // Genel hataların yakalandığı catch bloğu
+    {
+        Console.WriteLine("Bir hata oluştu");
+    }
+}
+
